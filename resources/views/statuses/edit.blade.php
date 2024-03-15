9c3413c41066c744cef5e16987a2d83b4f86cd49
Vladimir Ugarov <graciadan@rambler.ru>
2024-03-15 10:07:34 +0300

    working on templates

--- resources/views/statuses/edit.blade.php
+++ resources/views/statuses/edit.blade.php
@@ -5,9 +5,11 @@
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
             <div class="grid col-span-full">
                 <h1 class="mb-5 text-black dark:text-white text-5xl">{{ __('layout.task_status.edit_header') }}</h1>
+
                 @if ($errors->any())
                     @include('components.show-form-errors')
                 @endif
+
                 {{ Form::model($taskStatus, ['route' => ['task_statuses.update', $taskStatus], 'method' => 'PATCH', 'class' => 'w-50']) }}
                 <div class="flex flex-col">
                     <div>
@@ -17,7 +19,7 @@
                         {{ Form::text('name') }}
                     </div>
                     <div class="mt-2">
-                        {{ Form::submit(__('layout.button.create'), ['class' => 'bg-blue-500 hover:bg-blue-700
+                        {{ Form::submit(__('layout.button.update'), ['class' => 'bg-blue-500 hover:bg-blue-700
                         text-white font-bold py-2 px-4 rounded']) }}
                         <a class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
                            href="{{ route('task_statuses.index')}}">{{ __('layout.button.back') }}</a>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Mail;
 use App\Mail\OrderShipped;
 use App\Http\Controllers\TaskStatusController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\LabelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,3 +37,4 @@ require __DIR__ . '/auth.php';
 
 Route::resource('task_statuses', TaskStatusController::class);
 Route::resource('tasks', TaskController::class);
+Route::resource('labels', LabelController::class);
